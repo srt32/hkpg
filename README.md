3e6285651f599e74b54a0d6abacb9ebdc3d4ea0c
Simon Taranto <simon@backerkit.com>
2017-08-31 16:37:17 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# hkpg
+
+Automated Heroku Postgres backups written in go
+
+Inspired by https://github.com/kbaum/heroku-database-backups
---
+++ cmd/backup/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main () {
+  fmt.Println("hello world")
+  // get latest backup url
+  // download it
+  // upload it to s3
+  // report success
+}
