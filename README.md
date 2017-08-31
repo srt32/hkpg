eca9c55bc8bc60056b1025b0eecf980cc31b9d9e
Simon Taranto <simon@backerkit.com>
2017-08-31 17:03:22 -0600

    Fetch URL, download backup, upload to S3
    * End to end MVP

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Automated Heroku Postgres backups written in go
 
 Inspired by https://github.com/kbaum/heroku-database-backups
+
+Deploy with ideas in https://stackoverflow.com/a/43265218/1949363
--- cmd/backup/main.go
+++ cmd/backup/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-    "fmt"
+    "log"
+    "os"
 )
 
 func main () {
-  fmt.Println("hello world")
-  // get latest backup url
-  // download it
-  // upload it to s3
+  var herokuAuthToken = os.Getenv("HEROKU_AUTH_TOKEN")
+
+  if herokuAuthToken == "" {
+    log.Fatalf("HEROKU_AUTH_TOKEN must be set")
+  }
+
+  // -  get latest backup url
+    // https://github.com/bgentry/heroku-go
+    // 0. get token from env
+    // curl -X POST https://api.heroku.com/apps \
+    // -H "Accept: application/vnd.heroku+json; version=3" \
+    // -H "Authorization: Bearer $TUTORIAL_KEY"
+    // 1. GET /client/v11/apps/backerkit-staging2/transfers
+    // get entry with highest `num`
+    // 2. POST /client/v11/apps/backerkit-staging2/transfers/{highest_num}/actions/public-url
+    // pluck out `url` field
+
+  // - download it
+  // - upload it to s3
+    // https://github.com/aws/aws-sdk-go/tree/master/service/s3
+
   // report success
+    // call honeybadger (optionally)
 }
